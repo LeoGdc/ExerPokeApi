ec09c005209529c8c502fe1d05818f4297559d8e
LEONARDO DE NOVAES VIVI <21276367@CFP127ALUNO.LOCAL>
2022-03-30 16:12:14 -0300

    terminado o back

--- js/main.js
+++ js/main.js
@@ -1,61 +1,36 @@
 'use strict'
 
-let a = {
-    nome: "fernando",
-    idade: "40"
+const pesquisarPoke = () =>{
+
+    const getPokemonurl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+
+    for (let i =1; i<=151; i++){
+        pokemonPromises.push(fetch(getPokemonurl(i)).then(response => response.json()))
+    }
+    Promise.all(pokemonPromises)
+    .then(pokemons => {
+        const lisPokemons = pokemons.reduce((accumulator ,pokemon) => {
+
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            accumulator += 
+            `<div class="card">
+                <div class ="img">
+                <img class ="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+                </div>
+                <div class ="atributos">
+                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card=subtitle">${types.join(' | ')}</p>
+                </div>
+                
+            </div>`
+            return accumulator
+        }, '')
+        const ul = document.querySelector('[data-js="pokedex"]')
+       
+        ul.innerHTML = lisPokemons
+    })
 }
 
-const pesquisarRacas = async () => {
-
-    const url = 'https://pokeapi.co/api/v2/pokemon/'
-    const response = await fetch(url)
-    const data = await response.json()
-    return Object.keys(data.message)
-}
-
-const pesquisarCachorro = async (raca) =>{
-    const url = `https://pokeapi.co/api/v2/pokemon/${racas}`
-
-    const response = await fetch(url)
-
-    const data = await response.json()
-
-    return data
-
-}
-
-const criarImg = (imagem) =>{
-    const img =document.createElement('img')
-    img.src = imagem
-
-    return img
-
-}
-
-const carregarImagens = async () =>{
-    const container =document.getElementById('imagem-container')
-    const raca =document.getElementById('raca').value
-    const imagens = await pesquisarCachorro(raca)
-
-    const tagImagens = imagens.message.map(criarImg)
-
-    container.replaceChildren(...tagImagens)
-
-}
-
-const carregarRacas = async() =>{
-
-
-    const lista = document.getElementById('lista-racas')
-    const racas = await pesquisarRacas()
-    lista.innerHTML=`
-    <option>
-        ${racas.join("</options><option>")}
-    </otion>
-    `
-    console.log(racas.json("<option></option>"))
-
-}
-document.getElementById('pesquisar').addEventListener('click', carregarImagens)
-
-carregarRacas()
+pesquisarPoke()
